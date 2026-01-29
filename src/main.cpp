b24d2f57b360f23c67f453729827d630ce32cbdd
Roberto Nightingale Castillo <roberto.nightingale@hotmail.com>
2026-01-28 21:44:13 -0500

    we made a window, yay

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,12 @@
 #include <QApplication>
+#include "QMainWindow"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QMainWindow w;
+    w.show();
 
     return a.exec();
 }
